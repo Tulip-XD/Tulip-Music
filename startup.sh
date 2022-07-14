16357ec37f32c2987f65e7b00dd9b62ac71802c6
ITz_Tulip_XD <108618137+Tulip-XD@users.noreply.github.com>
2022-07-14 18:42:27 +0530

    Add files via upload

---
+++ startup.sh
@@ -0,0 +1,19 @@
+echo ">> FETCHING UPSTREAM..."
+git clone https://github.com/NarutoRepo/Naruto-Music /Naruto-Music
+echo ">> INSTALLING REQUIREMENTS..."
+cd /Naruto-Music
+pip3 install -U -r requirements.txt
+echo ">> STARTING MUSIC PLAYER USERBOT..."
+clear
+echo "
+#     #                        
+##   ## #    #  ####  #  ####  
+# # # # #    # #      # #    # 
+#  #  # #    #  ####  # #      
+#     # #    #      # # #      
+#     # #    # #    # # #    # 
+#     #  ####   ####  #  ####  
+                                                                          
+MUSIC PLAYER USERBOT IS SUCCESSFULLY DEPLOYED!
+"
+python3 main.py
