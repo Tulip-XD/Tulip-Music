98c296ca6d83287e54f3426ccdb22d6922bca519
ITz_Tulip_XD <108618137+Tulip-XD@users.noreply.github.com>
2022-07-14 18:54:35 +0530

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,7 +1,7 @@
 echo ">> FETCHING UPSTREAM..."
-git clone https://github.com/NarutoRepo/Naruto-Music /Naruto-Music
+git clone https://github.com/Tulip-XD/Tulip-Music /Tulip-Music
 echo ">> INSTALLING REQUIREMENTS..."
-cd /Naruto-Music
+cd /Tulip-Music
 pip3 install -U -r requirements.txt
 echo ">> STARTING MUSIC PLAYER USERBOT..."
 clear
